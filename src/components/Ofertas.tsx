15b801939f1feb88072d6af53b0b5f867da163c5
folows1 <michelribeirocorrea@gmail.com>
2022-07-16 22:04:18 -0300

    price filter

--- src/components/Ofertas.tsx
+++ src/components/Ofertas.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IPlanta } from "../interfaces/IPlanta";
 import Card from "./Card";
 import { ArrowUp, ArrowDown } from "phosphor-react";
+import classnames from "classnames";
 
 const mock: IPlanta[] = [
     {
@@ -38,7 +39,7 @@ const mock: IPlanta[] = [
 
 export default function Ofertas() {
 
-
+    const [priceRange, setPriceRange] = useState<'default' | '25' | '50' | 'max'>('default');
     const [plants, setPlants] = useState(mock);
     const [orderingPrice, setOrderingPrice] = useState(0);
     const [orderingAz, setOrderingAz] = useState(0);
@@ -73,6 +74,7 @@ export default function Ofertas() {
 
     function changeOrderingPrice(value: number) {
         setOrderingAz(0);
+        setPriceRange('default');
         if (value === 2) {
             setPlants(mock);
             setOrderingPrice(0);
@@ -84,6 +86,7 @@ export default function Ofertas() {
     }
 
     function changeOrderingAz(value: number) {
+        setPriceRange('default');
         setOrderingPrice(0);
         if (value === 2) {
             setPlants(mock);
@@ -96,35 +99,88 @@ export default function Ofertas() {
     }
 
     const priceOrder = [
-        <div onClick={() => changeOrderingPrice(0)} className='flex flex-row w-fit p-2 select-none items-center hover:cursor-pointer'>
+        <div onClick={() => changeOrderingPrice(0)} className='flex flex-row w-fit p-2 select-none items-center hover:cursor-pointer hover:border-b-2 border-orange-300'>
             Preço
             <ArrowDown size={28} />
         </div>,
-        <div onClick={() => changeOrderingPrice(1)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center'>
+        <div onClick={() => changeOrderingPrice(1)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center hover:border-b-2 border-orange-300'>
             Preço
             <ArrowDown size={28} />
         </div>,
-        <div onClick={() => changeOrderingPrice(2)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center'>
+        <div onClick={() => changeOrderingPrice(2)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center hover:border-b-2 border-orange-300'>
             Preço
             <ArrowUp size={28} />
         </div>,
     ]
 
     const azOrder = [
-        <div onClick={() => changeOrderingAz(0)} className='flex flex-row w-fit p-2 select-none items-center hover:cursor-pointer'>
+        <div onClick={() => changeOrderingAz(0)} className='flex flex-row w-fit p-2 select-none items-center hover:cursor-pointer hover:border-b-2 border-orange-300'>
             A-Z
             <ArrowDown size={28} />
         </div>,
-        <div onClick={() => changeOrderingAz(1)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center'>
+        <div onClick={() => changeOrderingAz(1)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center hover:border-b-2 border-orange-300'>
             A-Z
             <ArrowDown size={28} />
         </div>,
-        <div onClick={() => changeOrderingAz(2)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center'>
+        <div onClick={() => changeOrderingAz(2)} className='flex flex-row w-fit bg-orange-300 p-2 hover:cursor-pointer select-none items-center hover:border-b-2 border-orange-300'>
             A-Z
             <ArrowUp size={28} />
         </div>,
     ]
 
+    function priceRange25() {
+        return (
+            <div onClick={() => priceRange === '25' ? setPriceRange('default') : setPriceRange('25')}
+                className={classnames('flex flex-row w-[100px] hover:cursor-pointer select-none items-center p-2 justify-center hover:border-b-2 border-orange-300',
+                    { 'bg-orange-300': priceRange === '25' })}>
+                0 - 25
+            </div>
+        )
+    }
+
+    function priceRange50() {
+        return (
+            <div onClick={() => priceRange === '50' ? setPriceRange('default') : setPriceRange('50')}
+                className={classnames('flex flex-row w-[100px] hover:cursor-pointer select-none items-center p-2 justify-center hover:border-b-2 border-orange-300',
+                    { 'bg-orange-300': priceRange === '50' })}>
+                25 - 50
+            </div>
+        )
+    }
+
+    function priceRangeMax() {
+        return (
+            <div onClick={() => priceRange === 'max' ? setPriceRange('default') : setPriceRange('max')}
+                className={classnames('flex flex-row w-[100px] hover:cursor-pointer select-none items-center p-2 justify-center hover:border-b-2 border-orange-300',
+                    { 'bg-orange-300': priceRange === 'max' })}>
+                50 +
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        setOrderingAz(0);
+        setOrderingPrice(0);
+        const mockCopy = [...mock];
+        if (priceRange === 'default') {
+            setPlants(mockCopy);
+        }
+        if (priceRange === '25') {
+            setPlants(
+                mockCopy.filter(plant => plant.preco <= 25)
+            )
+        }
+        if (priceRange === '50') {
+            setPlants(
+                mockCopy.filter(plant => plant.preco > 25 && plant.preco <= 50)
+            )
+        }
+        if (priceRange === 'max') {
+            setPlants(
+                mockCopy.filter(plant => plant.preco > 50)
+            )
+        }
+    }, [priceRange]);
 
     return (
         <>
@@ -134,9 +190,14 @@ export default function Ofertas() {
                     <strong className="text-7xl font-black font-dif">ofertas</strong>
                 </div>
             </div>
-            <div className="flex flex-row max-w-[1200px] mx-auto mb-11 justify-center text-xl">
+            <div className="flex flex-row max-w-[1200px] mx-auto mb-11 justify-center text-xl h-[50px]">
                 {priceOrder[orderingPrice]}
                 {azOrder[orderingAz]}
+                <div className="flex flex-row ml-14 space-x-5">
+                    {priceRange25()}
+                    {priceRange50()}
+                    {priceRangeMax()}
+                </div>
             </div>
             <div className="grid grid-cols-3 gap-[30px] max-w-[1200px] mx-auto mb-[200px]">
                 {plants.map((item, index) => (
